1b57b05cb1541e01e4d9367bffe0ef25175c0077
devansh <devanshsaxena1019@gmail.com>
2023-09-14 01:41:49 +0530

    change again!

--- app/src/main/java/com/example/my_medicos/Contactinfo.java
+++ app/src/main/java/com/example/my_medicos/Contactinfo.java
@@ -1,14 +1,79 @@
 package com.example.my_medicos;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Contactinfo extends AppCompatActivity {
 
+    private Spinner location,speciality,subspeciality;
+
+    private ArrayAdapter<CharSequence> locationAdapter,specialityAdapter,subspecialityAdapter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contactinfo);
+
+        location = findViewById(R.id.location);
+        speciality = findViewById(R.id.speciality);
+        subspeciality = findViewById(R.id.subspeciality);
+
+        locationAdapter = ArrayAdapter.createFromResource(this,
+                R.array.indian_cities, android.R.layout.simple_spinner_item);
+        locationAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        location.setAdapter(locationAdapter);
+        // Initially, hide the subspeciality spinner
+        location.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+                // Check if the selected speciality has subspecialities
+                int locationIndex = location.getSelectedItemPosition();
+
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                // Do nothing
+            }
+        });
+        specialityAdapter = ArrayAdapter.createFromResource(this,
+                R.array.speciality, android.R.layout.simple_spinner_item);
+        specialityAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        speciality.setAdapter(specialityAdapter);
+        // Initially, hide the subspeciality spinner
+        speciality.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+                // Check if the selected speciality has subspecialities
+                int locationIndex = speciality.getSelectedItemPosition();
+
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                // Do nothing
+            }
+        });
+        subspecialityAdapter = ArrayAdapter.createFromResource(this,
+                R.array.subspeciality, android.R.layout.simple_spinner_item);
+        subspecialityAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        subspeciality.setAdapter(subspecialityAdapter);
+        // Initially, hide the subspeciality spinner
+        subspeciality.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+                // Check if the selected speciality has subspecialities
+                int locationIndex = subspeciality.getSelectedItemPosition();
+
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                // Do nothing
+            }
+        });
     }
 }
