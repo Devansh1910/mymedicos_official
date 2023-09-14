53f23892f31a250ef2ca0b1754c1f63bb0b17739
vivek <vivekpal2407@gmail.com>
2023-09-14 17:32:17 +0530

    profile

--- app/src/main/java/com/example/my_medicos/PostCmeActivity.java
+++ app/src/main/java/com/example/my_medicos/PostCmeActivity.java
@@ -3,9 +3,13 @@ package com.example.my_medicos;
 import android.app.DatePickerDialog;
 import android.app.ProgressDialog;
 import android.app.TimePickerDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
@@ -22,6 +26,8 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -41,6 +47,7 @@ import java.util.Locale;
 public class PostCmeActivity extends AppCompatActivity {
 
     FirebaseAuth auth = FirebaseAuth.getInstance();
+
     FirebaseUser user = auth.getCurrentUser();
     String current = user.getEmail();
 
@@ -62,11 +69,22 @@ public class PostCmeActivity extends AppCompatActivity {
     private TextView tvDate, tvTime;
     private Calendar calendar;
     private SimpleDateFormat dateFormat, timeFormat;
+    private static final int REQUEST_STORAGE_PERMISSION = 1;
+    private static final int REQUEST_STORAGE_ACCESS = 2;
+    private Button btnAccessStorage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_cme);
+        btnAccessStorage = findViewById(R.id.btnAccessStorage);
+
+        btnAccessStorage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                requestStoragePermission();
+            }
+        });
 
         btnDatePicker = findViewById(R.id.btnDatePicker);
         btnTimePicker = findViewById(R.id.btnTimePicker);
@@ -164,6 +182,63 @@ public class PostCmeActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void requestStoragePermission() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted, request it
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE},
+                    REQUEST_STORAGE_PERMISSION);
+        } else {
+            // Permission is already granted, open storage
+            openStorageForAccess();
+        }
+    }
+
+    private void openStorageForAccess() {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
+        startActivityForResult(intent, REQUEST_STORAGE_ACCESS);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_STORAGE_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted, open storage
+                openStorageForAccess();
+            } else {
+                // Permission denied, show a message or disable functionality
+                Toast.makeText(this, "Storage permission denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_STORAGE_ACCESS && resultCode == RESULT_OK) {
+            if (data != null) {
+                Uri treeUri = data.getData();
+                if (treeUri != null) {
+                    // Persist the URI for later use if needed
+                    // You can use treeUri to access the selected directory
+                    listFilesInDirectory(treeUri);
+                }
+            }
+        }
+    }
+
+    private void listFilesInDirectory(Uri treeUri) {
+        String treeDocumentId = DocumentsContract.getTreeDocumentId(treeUri);
+        Uri docUri = DocumentsContract.buildDocumentUriUsingTree(treeUri, treeDocumentId);
+
+        // Now, you can access and list files in the selected directory
+        // Perform file operations here
+        // You may need to use the DocumentsContract API to work with the documents
+    }
+
     private void showDatePicker() {
         DatePickerDialog datePickerDialog = new DatePickerDialog(
                 this,
